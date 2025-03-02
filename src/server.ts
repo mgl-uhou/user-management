83eb1c30ba1508b0e9c7caf916d7d987ee567cd3
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-03-02 20:56:42 -0300

    feat: Finalising Prisma

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
 import app from "./app";
+import prisma from "./database";
 
 app.listen(process.env.PORT, () => {
 	console.log(`http://localhost:${process.env.PORT}/api-docs`);
-})
+});
+
+// Graceful shutdown
+process.on("SIGINT", async () => {
+	console.log("SIGINT signal received: closing HTTP server");
+	await prisma.$disconnect(); // Remova ou comente esta linha se não estiver usando Prisma
+	process.exit(0);
+});
+
+process.on("SIGTERM", async () => {
+	console.log("SIGTERM signal received: closing HTTP server");
+	await prisma.$disconnect(); // Remova ou comente esta linha se não estiver usando Prisma
+	process.exit(0);
+});
