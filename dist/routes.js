9022a7586a6ecf95987e6e6e255bd42156ffbc91
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-03-29 20:29:23 -0300

    feat: using http-status-codes and refactoring

--- dist/routes.js
+++ dist/routes.js
@@ -1,11 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const http_status_codes_1 = require("http-status-codes");
 // import userController from "./controllers/UserController";
 const router = (0, express_1.Router)();
 // User Routes
 router.get("/", (_req, res) => {
-    res.status(200).send("Hello World!");
+    res.status(http_status_codes_1.StatusCodes.OK).send("Hello World!");
 });
 // router.post("/users", userController.create);
 exports.default = router;
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,12 @@
 import { Router, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 // import userController from "./controllers/UserController";
 
 const router = Router();
 
 // User Routes
 router.get("/", (_req: Request, res: Response) => {
-	res.status(200).send("Hello World!");
+	res.status(StatusCodes.OK).send("Hello World!");
 });
 
 // router.post("/users", userController.create);
