c5c0a670b47bdfe4c214973b823fd4e0b06bb9ff
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-03-29 19:52:40 -0300

    chore: fixing formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,21 +3,29 @@ import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default defineConfig([
-  globalIgnores([ "dist/**/*" ]),
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  tseslint.configs.recommended,
-  { rules: {
-    indent: [ "error", 2, { SwitchCase: 1 }],
-    semi: [ "error", "always" ],
-    quotes: [ "error", "double" ],
-    "linebreak-style": [ "error", "unix" ],
-    "no-tabs": [ "error", { allowIndentationTabs: false } ],
-    "eol-last": [ "error", "always" ],
-    "no-trailing-spaces": "error",
-    "@typescript-eslint/no-empty-interface": "off",
-  } },
+	globalIgnores(["dist/**/*"]),
+	{ files: ["**/*.{js,mjs,cjs,ts}"] },
+	{
+		files: ["**/*.{js,mjs,cjs,ts}"],
+		languageOptions: { globals: globals.browser },
+	},
+	{
+		files: ["**/*.{js,mjs,cjs,ts}"],
+		plugins: { js },
+		extends: ["js/recommended"],
+	},
+	tseslint.configs.recommended,
+	{
+		rules: {
+			indent: ["error", "tab", { SwitchCase: 1 }],
+			semi: ["error", "always"],
+			quotes: ["error", "double"],
+			"linebreak-style": ["error", "unix"],
+			"no-tabs": ["error", { allowIndentationTabs: true }],
+			"eol-last": ["error", "always"],
+			"no-trailing-spaces": "error",
+			"@typescript-eslint/no-empty-interface": "off",
+		},
+	},
 ]);
