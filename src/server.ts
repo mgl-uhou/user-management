74f95b206a2370a88fa2e15f9250b622b4319da5
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-03-29 14:22:26 -0300

    chore: removing comments

--- src/server.ts
+++ src/server.ts
@@ -8,12 +8,12 @@ app.listen(process.env.PORT, () => {
 // Graceful shutdown
 process.on("SIGINT", async () => {
 	console.log("SIGINT signal received: closing HTTP server");
-	await prisma.$disconnect(); // Remova ou comente esta linha se não estiver usando Prisma
+	await prisma.$disconnect();
 	process.exit(0);
 });
 
 process.on("SIGTERM", async () => {
 	console.log("SIGTERM signal received: closing HTTP server");
-	await prisma.$disconnect(); // Remova ou comente esta linha se não estiver usando Prisma
+	await prisma.$disconnect();
 	process.exit(0);
 });
