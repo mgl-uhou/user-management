ae21ac9fd7ee42d12cb78c2eda9a080795544e20
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-03-29 20:10:55 -0300

    chore: building updated

--- dist/app.js
+++ dist/app.js
@@ -16,7 +16,4 @@ app.use((0, cors_1.default)());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default));
 app.use(routes_1.default);
-app.get("/", (_req, res) => {
-    res.send("Hello World!");
-});
 exports.default = app;
--- dist/routes.js
+++ dist/routes.js
@@ -4,5 +4,8 @@ const express_1 = require("express");
 // import userController from "./controllers/UserController";
 const router = (0, express_1.Router)();
 // User Routes
+router.get("/", (_req, res) => {
+    res.status(200).send("Hello World!");
+});
 // router.post("/users", userController.create);
 exports.default = router;
